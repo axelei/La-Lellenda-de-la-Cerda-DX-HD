283c0fdac1df95d9942efd23c99f3cba4853c84e
altiereslima <altieres.lima@gmail.com>
2023-12-28 11:05:25 -0300

    Revert "teste"
    This reverts commit 5bf694103841c41898cb8fe26e78f3db92aecb31.

--- src/data/codepoint_to_diacritic.asm
+++
@@ -1,7 +0,0 @@
-;
-; Map codepoints (bytes of text as stored in the ROM) to diacritic tiles.
-;
-; See DialogCharmap, CodepointToTileMap
-;
-CodepointToDiacritic::
-    ; The English version doesn’t define any diacritics (but other revisions do).
